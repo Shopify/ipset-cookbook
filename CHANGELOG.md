610e7d4319df2d4af1ec56128221f9c5a61a5d27
Brad Ison <bison@garbagebrain.org>
2015-05-15 12:15:36 -0400

    Update metadata.rb and CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.2.0
+
+Remove `ipset_entry` LWRP.  Entries are now passed in a hash directly
+to the `ipset` resource.
+
 # 0.1.0
 
 Initial release of ipset
--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,10 @@ name             'ipset'
 maintainer       'Brad Ison'
 maintainer_email 'bison@garbagebrain.org'
 license          'Apache 2.0'
+source_url       'https://github.com/bison/ipset-cookbook'
 description      'Installs/Configures ipset on Linux'
-version          '0.1.0'
+version          '0.2.0'
+
+%w(debian ubuntu).each do |os|
+  supports os
+end
