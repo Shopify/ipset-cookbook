42ce40b41fe041a62b3a77fe670337c2e0ce69df
Brad Ison <bison@garbagebrain.org>
2015-05-26 12:23:07 -0400

    Bump version to 0.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bison@garbagebrain.org'
 license          'Apache 2.0'
 source_url       'https://github.com/bison/ipset-cookbook'
 description      'Installs/Configures ipset on Linux'
-version          '0.2.1'
+version          '0.2.2'
 
 %w(debian ubuntu).each do |os|
   supports os
